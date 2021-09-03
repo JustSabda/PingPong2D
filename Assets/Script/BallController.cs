c69e40a0723b59fb69bd756000bea1dc41ebf21d
JustSabda <dataerror13@gmail.com>
2021-09-03 18:00:14 +0700

    Bukti Belajar Part 5

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+    GameObject panelSelesai;
+    Text txPemenang;
+    public int force;
+    Rigidbody2D rigid;
+    int scoreP1;
+    int scoreP2;
+    Text scoreUIP1;
+    Text scoreUIP2;
+    AudioSource audio;
+    public AudioClip hitSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        panelSelesai = GameObject.Find("PanelSelesai");
+        panelSelesai.SetActive(false);
+        scoreP1 = 0;
+        scoreP2 = 0;
+        rigid = GetComponent<Rigidbody2D>();
+        Vector2 arah = new Vector2(2, 0).normalized;
+        rigid.AddForce(arah * force);
+        scoreUIP1 = GameObject.Find("Score1").GetComponent<Text>();
+        scoreUIP2 = GameObject.Find("Score2").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        audio.PlayOneShot(hitSound);
+        if (collision.gameObject.name == "TepiKanan")
+        {
+            scoreP1 += 1;
+            TampilkanScore();
+            ResetBall();
+            Vector2 arah = new Vector2(2, 0).normalized;
+            rigid.AddForce(arah * force);
+            if(scoreP1 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txPemenang = GameObject.Find("Pemenang").GetComponent<Text>();
+                txPemenang.text = "Player 1 Pemenang";
+                Destroy(gameObject);
+                return;
+            }
+        }
+        if (collision.gameObject.name == "TepiKiri")
+        {
+            scoreP2 += 1;
+            TampilkanScore();
+            ResetBall();
+            Vector2 arah = new Vector2(-2, 0).normalized;
+            rigid.AddForce(arah * force);
+            if(scoreP2 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txPemenang = GameObject.Find("Pemenang").GetComponent<Text>();
+                txPemenang.text = "Player 2 Pemenang";
+                Destroy(gameObject);
+                return;
+            }
+        }
+        if (collision.gameObject.name == "Pemukul1" || collision.gameObject.name == "Pemukul2")
+        {
+            float sudut = (transform.position.y - collision.transform.position.y) * 5f;
+            Vector2 arah = new Vector2(rigid.velocity.x, sudut).normalized;
+            rigid.velocity = new Vector2(0, 0);
+            rigid.AddForce(arah * force * 2);
+        }
+    }
+    void ResetBall()
+    {
+        transform.localPosition = new Vector2(0, 0);
+        rigid.velocity = new Vector2(0, 0);
+    }
+    void TampilkanScore()
+    {
+        Debug.Log("Score P1: " + scoreP1 + "Score P2: " + scoreP2);
+        scoreUIP1.text = scoreP1 + "";
+        scoreUIP2.text = scoreP2 + "";
+    }
+}
---
+++ Assets/Script/PaddleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public float kecepatan;
+    public float batasAtas;
+    public float batasBawah;
+    public string axis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gerak1 = Input.GetAxis(axis) * kecepatan * Time.deltaTime;
+        float nextPos1 = transform.position.y + gerak1;
+        if (nextPos1 > batasAtas)
+        {
+            gerak1 = 0;
+        }
+        if (nextPos1 < batasBawah)
+        {
+            gerak1 = 0;
+        }
+        transform.Translate(0, gerak1, 0);
+    }
+    
+}
